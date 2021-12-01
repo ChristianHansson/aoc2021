5408e08bd0dd183828051115bfff55c1bf1bd146
Christian <christian.hansson@askas.se>
2021-12-01 18:14:57 +0100

    part one

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const filehandler = require("./lib/file");
+const file_contents = require("./lib/file").load("one");
+const file_contents_arr = file_contents.split("\n");
+
+let increases = 0, previous = 0;
+for(const row of file_contents_arr){
+	const value = Number(row);
+	if (previous === 0){
+		previous = value;
+		continue;
+	}
+	if (value > previous){
+		increases++;
+	}
+
+	previous = value;
+}
+console.log(increases)
