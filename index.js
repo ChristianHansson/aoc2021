1142b9d8145d761c1539e69de75577cba25e4fce
Christian <christian.hansson@askas.se>
2021-12-05 13:55:17 +0100

    day3 part 2 done

--- index.js
+++ index.js
@@ -5,29 +5,65 @@ let legend = [];
 let len = 0;
 for (const line of arr){
 	legend.push(line.split(""));
-	len === 0 ? len = legend[0].length : !1;
+	len === 0 ? len = legend[0].length - 1 : !1;
 }
 
-
-const get_most_common_at_index = function(i){
-	let n = 0;
-	let l = {0: 0, 1: 0};
-	while (n < legend.length){
-		let num = legend[n++][i];
-		l[num]++;
+const remove_from_l_by_value_and_index = (value, index, l) => {
+	let i = 0;
+	let n = [];
+	while(i < l.length){
+		let row = l[i++];
+		if (+value === +row[index]){
+			n.push(row);
+		}
 	}
-	return l["0"] > l["1"] ? 0 : 1;
-}
+	return n;
+};
 
-let gamma = "";
-let epsilon = "";
-for (let i = 0; i < len; i++){
-	let most_common = get_most_common_at_index(i);
-	epsilon += most_common === 1 ? 0 : 1;
-	gamma += most_common;
-}
-gamma = parseInt(gamma, 2);
-epsilon = parseInt(epsilon, 2);
-const consumption = gamma * epsilon;
-console.log(gamma, epsilon, consumption);
-// 1337 2758 3687446 Correct!
+const parse_array = (index, l, keep_one_with_most = true) => {
+	let i = 0;
+	let o = {1: 0, 0: 0};
+	let splice_at_indexes = -1;
+	while (i < l.length){
+		let char = l[i++][index];
+		if (!(char in o)){
+			console.log(char)
+			process.exit()
+		}
+		o[char]++;
+	}
+	if (l.length === 1){
+		return l;
+	}
+	if (keep_one_with_most){
+		if (o["1"] > o["0"]){
+			l = remove_from_l_by_value_and_index("1", index, l);
+		}else if (o["0"] > o["1"]){
+			l = remove_from_l_by_value_and_index("0", index, l);
+		}else{
+			l = remove_from_l_by_value_and_index("1", index, l);
+		}
+	}else{
+		if (o["1"] > o["0"]){
+			l = remove_from_l_by_value_and_index("0", index, l);
+		}else if (o["0"] > o["1"]){
+			l = remove_from_l_by_value_and_index("1", index, l);
+		}else{
+			l = remove_from_l_by_value_and_index("0", index, l);
+		}
+	}
+	return index < len ? parse_array(index + 1, l, keep_one_with_most) : l;
+};
+
+let oxygen_value = parse_array(0, legend);
+let oxygen_value_decimal = parseInt(oxygen_value[0].join(""), 2);
+console.log(`oxygen_value: ${oxygen_value_decimal}`);
+let co_two_value = parse_array(0, legend, false);
+let co_two_value_decimal = parseInt(co_two_value[0].join(""), 2);
+console.log(`co_two_value: ${co_two_value_decimal}`);
+const life_support_rating = oxygen_value_decimal * co_two_value_decimal;
+console.log(`life_support_rating: ${life_support_rating}`);
+
+// oxygen_value: 1599
+// co_two_value: 2756
+// life_support_rating: 4406844 Correct!
