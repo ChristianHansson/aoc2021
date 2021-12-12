dad20e41673c05fedd1793991682f90c174c731a
Christian <christian.hansson@askas.se>
2021-12-12 13:51:30 +0100

    m

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const filecontent = require("./lib/file").load("data");
 const log = function(){console.log.apply(null, arguments);};
+const fs = require("fs"), path = require("path");
+const is_debug = Boolean(process.env.DEBUG) === true;
+const debug = function(content, first = false){
+	if (!is_debug){return;}
+	first === false ? fs.appendFileSync(path.resolve(__dirname, 'debug.txt'), content) : fs.writeFileSync(path.resolve(__dirname, 'debug.txt'), content);
+};
